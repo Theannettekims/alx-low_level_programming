e783e53ba8747245c95f199d289a9fec19080bda
Theannettekims <akimwere@gmail.com>
2022-07-28 11:16:46 -0700

    my commit message

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * malloc_checked - Entry Point
+ * @b: input amount
+ * Return: pointer to new mem
+ */
+{
+	void *n;
+
+	n = malloc(b);
+
+	if (n == NULL)
+		exit(98);
+	return (n);
+}
