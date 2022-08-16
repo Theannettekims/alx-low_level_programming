b90e3a64d40bd8783d519689a7265b0f26bc6464
Theannettekims <akimwere@gmail.com>
2022-08-15 22:56:58 -0700

    a function that adds a new line at the end of a node

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,7 +4,7 @@
  * add_nodeint_end - adds a new node at the end of a listint_t list
  * @head: double pointer to the head of list
  * @n: integer to be added
- * Return: address of node added
+ * Return: address of new element
  */
 listint_t *add_nodeint_end(listint_t **head, const int n);
 {
