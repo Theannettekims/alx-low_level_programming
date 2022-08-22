e6d537e87063440b306671b8279208aa31d62954
Theannettekims <akimwere@gmail.com>
2022-08-22 14:12:54 -0700

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (len = 0; text_content[len];
 				len++;
-				}
+	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
@@ -30,4 +30,4 @@ int create_file(const char *filename, char *text_content)
 	close(o);
 
 	return (1);
-	}	
+}	
