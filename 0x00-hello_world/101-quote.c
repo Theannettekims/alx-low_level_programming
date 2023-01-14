5e571b35d6f947257df6b92def2a46936383b593
Theannettekims <akimwere@gmail.com>
2023-01-14 07:00:35 -0800

    printing to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** main - Entry point
+ *
+ * return 1 (success)
+ */
+int main
+{
+	fprintf(stderr, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19)
+		return (1);
+}
