d7054780880291fed0305f1d70e9ad3e09a5af5a
Theannettekims <akimwere@gmail.com>
2023-09-06 11:03:14 -0700

    function that prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,14 +9,11 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		putchar('\n');
-		return;
-	}
+	if (*s != '\0')
+		_print_rev_recursion(s + 1);
 	else
 	{
-		putchar(*s);
-		_print_rev_recursion("s + 1");
+		return;
+		putchar(*s);		
 	}
 }
