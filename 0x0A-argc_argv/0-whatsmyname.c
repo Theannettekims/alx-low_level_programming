ae99f31fd210b758506fa568374631af91f0b588
Theannettekims <akimwere@gmail.com>
2023-09-10 13:03:48 -0700

    a program that prints its name followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argv - command line argument
+ * @argc - command line argument
+ * return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("my name is: %s",argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+this README file is for the argc and argv command line arguments.
+we take a look at how this arguments are passed and used in the main function
