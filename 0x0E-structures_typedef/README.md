5d2a13d12faceafb550242d991a3694096a7fa4d
Theannettekims <akimwere@gmail.com>
2023-09-11 12:43:19 -0700

    defining a structure

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+in this directory, we look at structures. there declaration,initiliasation,accessing and pointers to structures. we shall also look at typedef and what it does.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * struct dog - structure type with elements
+ * @name: structure element of character type
+ * @age: structure element of float type
+ * @owner: structure element of character type
+ * Return: 0
+ */
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+};
