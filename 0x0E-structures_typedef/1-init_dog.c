714d2ee791f89df86823073b82fcb8a5174b4f05
Theannettekims <akimwere@gmail.com>
2023-09-11 13:32:46 -0700

    initialising a structure variable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * struct dog *d - structure with pointer variable
+ * struct dog *d - structure of type struct dog with pointer variable
  *@i: input
  *
  * Return: 0
  */
 
-int i;
+void init_dog(struct dog *d, char *name, float age, char *owner); int i;
 struct dog *d
 {
 	for(i = 0;i < *d;i++)
