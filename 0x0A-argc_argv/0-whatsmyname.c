b2b0cec26bb56788eae582b27018456c3004caee
Theannettekims <akimwere@gmail.com>
2023-09-11 04:47:08 -0700

    print programme name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - prints name + \n
  * @argv: array pointing to arguments
  * @argc: number of arguments typed
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
 	argc = 0;
